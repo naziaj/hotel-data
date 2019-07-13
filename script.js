09d12e2f931d80149484ceea3dd39826c7c60611
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-13 11:32:15 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,55 @@
+let hotel ={
+    name:"PC hotel",
+    Address: "Moulvi Tamizuddin Khan Rd, Lalazar Karachi, Karachi City, Sindh",
+    Phone: (021)-35611031,
+    freeWifi:"in all rooms"
+    gardens:
+    roomsAndSuites:[
+        {
+            standardRooms:
+            superiorRooms:
+            deluxeRooms:
+            standardSuites:
+            deluxeSuites:
+            beachGrandSuites;
+        }
+    ]
+    halls:["jasmine hall", "tulip hall","rose hall", "sunflower hall","marigold hall", "daffodil hall"]
+    lawns:
+    meetingRooms:
+    employees:[
+        {
+            ladies:
+            gents:
+        }
+    ]
+    waiters:
+    staff:[
+
+    ]
+    guards:
+    DJ:
+    menu:[
+        {
+            continentalFood:[
+                {}
+            ]
+            italianFood:[
+                {}
+            ]
+
+        }
+    ]
+    chairs:[{
+
+    }]
+    tables:
+    airconditon:
+    waterCoolers:
+    coolers:
+    dustbins:
+}
+
+
+
+
