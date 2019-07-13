10eb7f90d6ff3d66623cf564973431af0f446c0d
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-13 17:16:21 +0500

    WIP

--- script.js
+++ script.js
@@ -1,54 +1,186 @@
 let hotel ={
-    name:"PC hotel",
+    name:"Beach Luxury Hotel",
     Address: "Moulvi Tamizuddin Khan Rd, Lalazar Karachi, Karachi City, Sindh",
     Phone: (021)-35611031,
-    freeWifi:"in all rooms"
-    gardens:
+    ceo:"",
+    CA:"",
+    supervisor:'',
+    manager:"",
+    Wifi:"in all rooms",
+    gardens:"",
+    pots: 300,
+    flowers:'',
     roomsAndSuites:[
         {
-            standardRooms:
-            superiorRooms:
-            deluxeRooms:
-            standardSuites:
-            deluxeSuites:
-            beachGrandSuites;
+            standardRooms:["","","",],
+            superiorRooms:["","",""],
+            deluxeRooms:["","",""],
+            standardSuites:["","",""],
+            deluxeSuites:["",""],
+            beachGrandSuites:["",''],
         }
-    ]
-    halls:["jasmine hall", "tulip hall","rose hall", "sunflower hall","marigold hall", "daffodil hall"]
-    lawns:
-    meetingRooms:
-    employees:[
-        {
-            ladies:
-            gents:
-        }
-    ]
-    waiters:
+    ],
+    halls:["jasmine hall", "tulip hall","rose hall", "sunflower hall","marigold hall", "daffodil hall"],
+    lawns:[],
+    meetingRooms:[],
+    employees:{
+        wiaters:[],
+        sweepers:[],
+        guards:[],
+        chefs:[],
+        counters:[],
+        accountant:[],
+        receptionist:[],
+    },
+        
+    
     staff:[
 
-    ]
-    guards:
-    DJ:
+    ],
+    
+    DJ:"",
     menu:[
         {
             continentalFood:[
-                {}
-            ]
-            italianFood:[
-                {}
+                {
+                    name: "the Gobler",
+                    cost: 800,
+                },
+                {
+                dishName: "asdmkam",
+                cost:234,
+                flavors:
+
+            }
+                
+
+            ],
+            italianFood:[{
+                name:"pizza",
+                cost:500,
+                sizesOfPizza:["la]
+                varities:[{
+                    varity1:"abc pizza",
+
+                }]
+            }
+                "pizza","spaghetti","jhdhg","dfggnj","dghgb",
+            ],
+            deserts:[
+                "banana desert","sdfhgufdh","gfhzf",
             ]
 
         }
-    ]
-    chairs:[{
+    ],
+    chairs:[
+        {
+          noOfBlueChairs:1000,
+          whiteChairs:1000,
+          roundchairs:5000,
+        }
+    ],
+    tables:[
+        {
+            roundTables:100,
+            goldenTables:100,
+            smallTables:40;
+        }
+    ],
+    sofa:[
+        {
 
-    }]
-    tables:
-    airconditon:
-    waterCoolers:
-    coolers:
-    dustbins:
+        }
+    ]
+    airconditon:"in all rooms",
+    waterCoolers: "in all rooms",
+    coolers:" in all rooms",
+    dustbins:"in all rooms",
+    soap:"in all rooms",
+    shampoo:"in all rooms",
+    crockery:{
+        plates: 2000,
+        cups: 2000,
+        largePlates:2000,
+        bowls: 2000,
+        other:'',
+    },
+    utilities:[
+        {
+            electricityBill:00000,
+            waterBill:000000,
+            gasBill:0000002,
+            telephonesBill:1243545,
+            wifiBill:3254657,
+            taxes:678774,
+        }
+    ],
+    grocery:{
+        vegetables:["fg","kjgh","ghjh","fgn"],
+        fruits:["fg","kjgh","ghjh","fgn"],
+        pulses:["fg","kjgh","ghjh","fgn"],
+        grains:["fg","kjgh","ghjh","fgn"],
+        wheat:["fg","kjgh","ghjh","fgn"],
+        spices:["fg","kjgh","ghjh","fgn"],
+        juices:["fg","kjgh","ghjh","fgn"],
+        drinks:["fg","kjgh","ghjh","fgn"],
+    },
+salaryExpenses:[
+    {
+        ceo:00000000,
+        CA:000000,
+        manager:0000000,
+        accountant:00000,
+        receptionists:{
+            dxsbc:245668,
+            ahsn:12345,
+            sndw:123445,
+            sjdbhbd:12345,
+            
+        },
+
+        staffSalarires:{
+            dxsbc:245668,
+            ahsn:12345,
+            sndw:123445,
+            sjdbhbd:12345,
+            ajshaih:56978,
+            vkncbkjb:123456,
+            },
+        sweepingStaffSlaries:{
+            dxsbc:245668,
+            ahsn:12345,
+            sndw:123445,
+            sjdbhbd:12345,
+            ajshaih:56978,
+            vkncbkjb:123456,
+        },
+        guardsSalary:{
+            dxsbc:245668,
+            ahsn:12345,
+            sndw:123445,
+            sjdbhbd:12345,
+            ajshaih:56978,
+            vkncbkjb:123456,
+        },
+        otheremplyeesSalaries:{
+            dxsbc:245668,
+            ahsn:12345,
+            sndw:123445,
+            sjdbhbd:12345,
+            ajshaih:56978,
+            vkncbkjb:123456,
+        },
+
+         }
+
+]
+
 }
+            
+
+    
+
+
 
 
 
